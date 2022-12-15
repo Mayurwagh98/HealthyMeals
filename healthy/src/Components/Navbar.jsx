298881a5c8a1214eb71e3d67f0fb3d47895f8a08
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-15 14:26:04 +0530

    Added: Navbar

---
+++ healthy/src/Components/Navbar.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+import { Menu } from "antd";
+import {CommonDrawer} from "./Drawer"
+
+
+function getItem(label, key, icon, children, type) {
+  return {
+    key,
+    icon,
+    children,
+    label,
+    type,
+  };
+}
+
+const items = [
+  getItem("Navigation One", "sub1", <MailOutlined />, [
+    getItem(
+      "Pages",
+      "g1",
+      null,
+      [getItem("Home", "/"), getItem("Meals", "/meals")],
+      "group"
+    ),
+    // getItem(
+    //   "Item 2",
+    //   "g2",
+    //   null,
+    //   [getItem("Option 3", "3"), getItem("Option 4", "4")],
+    //   "group"
+    // ),
+  ]),
+  getItem("Navigation Two", "sub2", <AppstoreOutlined />, [
+    getItem(<CommonDrawer />,""),
+    getItem("Option 6", "6"),
+    getItem("Submenu", "sub3", null, [
+      getItem("Option 7", "7"),
+      getItem("Option 8", "8"),
+    ]),
+  ]),
+  getItem("Navigation Three", "sub4", <SettingOutlined />, [
+    getItem("Option 9", "9"),
+    getItem("Option 10", "10"),
+    getItem("Option 11", "11"),
+    getItem("Option 12", "12"),
+  ]),
+];
+
+let Navbar = () => {
+  let navigate = useNavigate();
+  const onClick = (e) => {
+    console.log("click ", navigate(e.key));
+  };
+  return (
+    <Menu
+      onClick={onClick}
+      style={{
+        width: 256,
+        // border:"1px solid red"
+      }}
+      defaultSelectedKeys={["1"]}
+      defaultOpenKeys={["sub1"]}
+      mode="inline"
+      items={items}
+    />
+  );
+};
+
+export { Navbar };
