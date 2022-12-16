70fe2fee370f37c29ad9dfc8c4f263f88d02cb92
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-16 17:53:32 +0530

    Added: Quantity Functionality

--- healthy/src/Components/OrderPage.jsx
+++ healthy/src/Components/OrderPage.jsx
@@ -2,10 +2,34 @@ import React, { useState } from "react";
 import { Card, Collapse, Button } from "antd";
 
 let Order = () => {
-  let OrderedDetails = JSON.parse(localStorage.getItem("OrderedItem"));
+  let OrderedDetails = JSON.parse(localStorage.getItem("CartDetails")) || [];
+  let [displayOrder, setDisplayOrder] = useState([...OrderedDetails]);
+
+
   const { Meta } = Card;
 
   const { Panel } = Collapse;
+
+  let handleQty = (val, item) => {
+    if (val === "Add") {
+      item.qty++;
+    } else if (val === "Sub") {
+      item.qty--;
+    }
+
+    localStorage.setItem("CartDetails", JSON.stringify([...OrderedDetails]));
+    setDisplayOrder([...OrderedDetails]);
+  };
+  // ----------------------- Delete Item-------------------------------------------
+
+  let handleDelete = (index) =>{
+
+    OrderedDetails.splice(index, 1)
+
+    localStorage.setItem("CartDetails", JSON.stringify(OrderedDetails))
+
+    setDisplayOrder([...OrderedDetails])
+  }
   return (
     <>
       {/* <h1>Hi</h1> */}
@@ -22,18 +46,19 @@ let Order = () => {
               cover={<img alt="example" src={item.strMealThumb} />}
             >
               <Panel header={item.strMeal} key="0">
-                <Meta
-                  // title={item.strMeal}
-                  //adding 1 because to display price from 1 to 1000 (as a whole number)
-                  //   description={`₹ ${Math.floor(Math.random() * 1000) + 1}`}
-                  style={{ textAlign: "center" }}
-                />
-
+                <Meta style={{ textAlign: "center" }} />
               </Panel>
-                <Button onClick={() => handleQty("Add")}>+</Button>
-                <h3>0</h3>
-                <Button onClick={() => handleQty("Sub")}>-</Button>
+              <Button onClick={() => handleQty("Add", item)}>+</Button>
+              <h3>{item.qty}</h3>
+              <Button
+                onClick={() => handleQty("Sub", item)}
+                disabled={item.qty == 1}
+              >
+                -
+              </Button>
+              <Button type="primary" onClick={() => handleDelete(index)}>Delete</Button>
             </Card>
+            
           );
         })}
       </div>
