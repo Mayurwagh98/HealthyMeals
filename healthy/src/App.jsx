d209ba1175d2c05e97559a3224c483164b6c8749
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-20 23:28:02 +0530

    Added: Route to Pages

--- healthy/src/App.jsx
+++ healthy/src/App.jsx
@@ -3,7 +3,10 @@ import { Meals } from "./Components/Meals";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./Components/Home";
 import { Navbar } from "./Components/Navbar";
-// import {Signup} from "./Components/Signup"
+import { Login } from "./Components/Login";
+import { Order } from "./Components/OrderPage";
+import { Error } from "./Components/Error";
+import { Payment } from "./Components/Payment";
 
 function App() {
   return (
@@ -24,6 +27,10 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/meals" element={<Meals />} />
           {/* <Route exact path="/signup" element={<Signup />} /> */}
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/order" element={<Order />} />
+          <Route exact path="/payment" element={<Payment />} />
+          <Route exact path="*" element={<Error />} />
         </Routes>
       </div>
     </div>
