9462dddb13413bec00e58a7310e481eafbe92183
Mayurwagh98 <mswagh1998@gmail.com>
2022-12-27 18:50:49 +0530

    Added: Slidshow

--- healthy/src/Components/Home.jsx
+++ healthy/src/Components/Home.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Card,message  } from "antd";
-import { Loader } from "./Loading";
+import { Card, message } from "antd";
+import { Cor } from "./Slider";
 
 let Home = () => {
   let [homeData, setHomeData] = useState([]);
   let [homeData2, setHomeData2] = useState([]);
   let [loading, setLoading] = useState(true);
+  // ---------------------- Loading ---------------------------------
   const [messageApi, contextHolder] = message.useMessage();
-  const key = 'updatable';
+  const key = "updatable";
   const openMessage = () => {
     messageApi.open({
       key,
-      type: 'loading',
-      content: 'Loading...',
+      type: "loading",
+      content: "Loading...",
     });
     setTimeout(() => {
       messageApi.open({
         key,
-        type: 'success',
-        content: 'Loaded!',
+        type: "success",
+        content: "Loaded!",
         duration: 2,
       });
     }, 1000);
   };
+  // --------------------------- Getting the Data-----------------------------------
   let getData = async () => {
     await axios
       .get(`https:/www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`)
@@ -32,7 +34,7 @@ let Home = () => {
         homeData = res.data.meals;
         setTimeout(() => {
           setLoading(false);
-          }, 1000);
+        }, 1000);
         setHomeData(homeData);
         console.log(homeData);
       })
@@ -59,13 +61,22 @@ let Home = () => {
 
   return (
     <>
-    {contextHolder}
-    <h1 style={{ textAlign: "center" }}>Famous Meals</h1>
+      {contextHolder}
+      <div className="logo_div">
+        <img
+          src="https://user-images.githubusercontent.com/69896733/209149712-53ed17eb-c85b-4939-bf2e-07b66553a960.png"
+          alt="image_logo"
+          className="logo_img"
+        />
+      </div>
+      <div>
+        <Cor />
+      </div>
+      <h1 style={{ textAlign: "center" }}>Famous Meals</h1>
       {loading ? (
         // <Loader />
         openMessage()
-
-        ) : (
+      ) : (
         <div className="home_meals">
           {homeData.map((item, index) => {
             return (
@@ -73,7 +84,7 @@ let Home = () => {
                 hoverable
                 style={{
                   width: 220,
-                  margin:"auto"
+                  margin: "auto",
                 }}
                 key={index}
                 cover={<img alt="example" src={item.strMealThumb} />}
@@ -92,7 +103,6 @@ let Home = () => {
       {loading ? (
         // <Loader />
         openMessage()
-
       ) : (
         <div className="home_meals">
           {homeData2.map((item, index) => {
@@ -105,7 +115,7 @@ let Home = () => {
                 hoverable
                 style={{
                   width: 220,
-                  margin:"auto"
+                  margin: "auto",
                 }}
                 key={index}
                 cover={<img alt="example" src={item.strCategoryThumb} />}
