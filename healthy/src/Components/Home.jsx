8702c29b2de23322439e62a38c876913a3ea9cb7
Mayurwagh98 <mswagh1998@gmail.com>
2023-01-03 06:28:18 +0530

    Removed: Logo img

--- healthy/src/Components/Home.jsx
+++ healthy/src/Components/Home.jsx
@@ -62,13 +62,7 @@ let Home = () => {
   return (
     <>
       {contextHolder}
-      <div className="logo_div">
-        <img
-          src="https://user-images.githubusercontent.com/69896733/209149712-53ed17eb-c85b-4939-bf2e-07b66553a960.png"
-          alt="image_logo"
-          className="logo_img"
-        />
-      </div>
+     
       <div>
         <Cor />
       </div>
